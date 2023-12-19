d381e58b4f72ad2ca9f863dc7410f43ba7ae5878
Cristian Ritter <cristianritter@gmail.com>
2023-12-19 13:19:48 -0300

    atualizacoes para exibicao mobile

--- catalogodigital/settings.py
+++ catalogodigital/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-)cb+5%vn8)pa84q4#_*t(lv)5pjs)_nvo$a@iuh@p1k9$uj6t(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '192.168.2.130']
 
 
 # Application definition
