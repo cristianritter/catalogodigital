3feaebc3f41cd314ba5666861a72751d99c89613
Cristian Ritter <cristianritter@gmail.com>
2024-01-09 18:07:30 -0300

    adicao de config settings ssl

--- catalogodigital/settings.py
+++ catalogodigital/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)cb+5%vn8)pa84q4#_*t(lv)5pjs)_nvo$a@iuh@p1k9$uj6t(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+DJANGO_SECURE_SSL_REDIRECT = False
 ALLOWED_HOSTS = ['*', '192.168.2.130']
 
 
