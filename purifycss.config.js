9dec5262cf0f0cee3167cdc7dd585ecfc6c6bb96
Cristian Ritter <cristianritter@gmail.com>
2024-01-10 20:04:32 -0300

    avoiding invisible text

---
+++ purifycss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    paths: ['D:\VSCODE_WORKSPACE\catalogodigital\landing\templates\cities\sapiranga\*.html', 'landing\static\common\js\*.js'],
+    css: ['D:\VSCODE_WORKSPACE\catalogodigital\landing\static\common\css\styles.css'],
+    output: 'D:\VSCODE_WORKSPACE\catalogodigital\landing\static\common\css\cleaned-styles.css',
+};
