7a61914a93dacb095a57d633d43e104a2a102d6b
Cristian Ritter <cristianritter@gmail.com>
2024-01-13 20:16:01 -0300

    Revert "adicao rotas index e home"
    This reverts commit 3fa574840a23a8fd29d4550754b312f06b665f59.

--- landing/urls.py
+++ landing/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from .views import CatalogoDigital, AJRCutelaria
 
 urlpatterns = [
-    path('', CatalogoDigital.as_view(), name='catalogodigital'),
-    path('index', CatalogoDigital.as_view(), name='catalogodigital'),
-    path('home', CatalogoDigital.as_view(), name='catalogodigital'),
     path('catalogodigital', CatalogoDigital.as_view(), name='catalogodigital'),
     path('ajrcutelaria', AJRCutelaria.as_view(), name='ajrcutelaria'),
     
