439fa9beceef0981abd92fe7ccabdb86ee8565c3
Cristian Ritter <cristianritter@gmail.com>
2024-01-13 20:34:21 -0300

    mudancas no deploy databases

--- catalogodigital/settings.py
+++ catalogodigital/settings.py
@@ -109,11 +109,12 @@ WSGI_APPLICATION = "catalogodigital.wsgi.application"
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }"""
+import dj_database_url
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')))
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
