9d1b28a65e219e74b924c57415bf96f41ace6402
Cristian Ritter <cristianritter@gmail.com>
2024-01-12 21:25:14 -0300

    teste com whitenoise

--- catalogodigital/settings.py
+++ catalogodigital/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 WHITENOISE_USE_BROTLI = True
 
 # Default primary key field type
