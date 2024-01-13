9f1444502a90f5578327ea2cb5f653c61fad823d
Cristian Ritter <cristianritter@gmail.com>
2024-01-13 13:09:56 -0300

    organizacao e implementacao do template padrao

--- landing/urls.py
+++ landing/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.index, name='index'),
+    path('catalogodigital', views.index, name='index'),
     path('ajrcutelaria', views.ajrcutelaria, name='ajrcutelaria'),
     path('residencialvivatorres', views.residencialvivatorres, name='residencialvivatorres'),
     
