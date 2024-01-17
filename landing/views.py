6d1b649fef24bab33cb1628b546a200035e08437
Cristian Ritter <cristianritter@gmail.com>
2024-01-17 11:05:56 -0300

    correcoes nos templates, adicionado email, dados condicionais

--- landing/views.py
+++ landing/views.py
@@ -72,9 +72,9 @@ class CatalogoDigital(BaseLandPage):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.context = {
-            'description' : 'Desenvolvimento de landing page para negócios, marketing e divulgação organica',
-            'window_title': 'Catálogo Digital - Seu negócio na Web',
             'static_assets_path': 'cities/florianopolis/catalogodigital/assets',
+            'meta_description' : 'Desenvolvimento e hospedagem de landing pages para negócios, marketing e divulgação organica',
+            'window_title': 'Catálogo Digital - Seu negócio na Web',
             'sobretitulo': 'Bem Vindo ao Catálogo Digital!',
             'titulo': 'A melhor landing page para o seu negócio',
             'body_title': 'O que é uma landing page?',
@@ -89,18 +89,20 @@ class CatalogoDigital(BaseLandPage):
             'promocao_content': 'Informe que nos encontrou através do nosso site e ganhe 10% de desconto no valor da sua primeira anuidade!',
             'horario_atendimento': 'Seg à Sex das 07-19h.',
             'telefones': '(48) 996810518',
-            'endereco': 'Rua Duque de Caxias, 634, Centenário, Sapiranga, RS',
             'whatslink': 'https://wa.me/+5551996810518',
-            'gmaps_embed_link': 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3468.0310528904797!2d-51.01371158831997!3d-29.631841339516633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95193fbe62929801%3A0xfb885bf48d7148d6!2sAJR%20Cutelaria!5e0!3m2!1sen!2sbr!4v1703100728491!5m2!1sen!2sbr',
+            'email':'cristianritter@gmail.com', 
+            'google_share_link': '',
+            'endereco': '',
+            'gmaps_embed_link': '',
         }
 
 class KelliSenaAcessoria(BaseLandPage):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.context = {
-            'description' : 'Landing page assessoria juridica atendimento em todo o pais, são paulo.',
-            'window_title': 'Kelli Sena - Assessoria trabalhista e financeira',
             'static_assets_path': 'cities/saopaulo/kellisenaassessoria/assets',
+            'meta_description' : 'Landing page de Assessoria trabalhista e financeira em São Paulo no Alto do Ipiranga, atende todo o Brasil.',
+            'window_title': 'Kelli Sena - Assessoria trabalhista e financeira',
             'sobretitulo': 'Assessoria trabalhista e financeira',
             'titulo': 'Kelli Sena',
             'item1_title': 'Nossa História',
@@ -110,22 +112,23 @@ class KelliSenaAcessoria(BaseLandPage):
             'item3_title': 'Nosso Diferencial',
             'item3_content': 'Assessoria jurídica especializada com expertise legislativa, cálculos precisos e atendimento personalizado.',
             'promocao_titulo': 'Aproveite nosso desconto exclusivo!',
-            'promocao_content': 'Informe que conheceu o nosso local através do nosso site e ganhe 5% de desconto no valor total da sua primeira compra! (Limitado à R$20)',
+            'promocao_content': 'Informe que conheceu o nosso local através do nosso site e ganhe 5% de desconto no valor total do seu primeiro serviço!5',
             'horario_atendimento': 'Seg à Sex das 09-17h.',
             'telefones': '(11) 95296-4549',
-            'endereco': 'Alto Ipiranga, São Paulo, SP',
+            'email':'', 
             'whatslink': 'https://wa.me/+5511952964549',
-            'gmaps_embed_link': '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10341.049867764943!2d-46.62931362676512!3d-23.600271298884838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5ba9cdaeaf81%3A0x8cccb9a54c7a0280!2sIpiranga!5e0!3m2!1sen!2sbr!4v1705458908604!5m2!1sen!2sbr"></iframe>',
+            'google_share_link': 'https://maps.app.goo.gl/pTZvag2fg7ytV74eA',
+            'endereco': '',
+            'gmaps_embed_link': '',
         }
-
 
 class AJRCutelaria(BaseLandPage):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.context = {
-            'description' : 'Landing page comercio local afiação amolador alicates de unha tesouras facas.',
-            'window_title': 'Adelcio Afiador - Afiação e Venda de Ferramentas para Salões de beleza',
             'static_assets_path': 'cities/sapiranga/ajrcutelaria/assets',
+            'meta_description' : 'Landing page comercio local afiação amolador alicates de unha tesouras facas em Sapiranga Rio Grande do Sul.',
+            'window_title': 'Adelcio Afiador - Afiação e Venda de Ferramentas para Salões de beleza',
             'sobretitulo': 'Afiação e Venda de Ferramentas para Salões de beleza',
             'titulo': 'Adélcio Amolador',
             'item1_title': 'Nossa História',
@@ -138,8 +141,10 @@ class AJRCutelaria(BaseLandPage):
             'promocao_content': 'Informe que conheceu o nosso local através do nosso site e ganhe 10% de desconto no valor total da sua primeira compra! (Limitado à R$20)',
             'horario_atendimento': 'Seg à Sex das 07-19h.',
             'telefones': '(51) 980159178',
-            'endereco': 'Rua Duque de Caxias, 634, Centenário, Sapiranga, RS',
+            'email':'', 
             'whatslink': 'https://wa.me/+5551980159178',
+            'google_share_link': 'https://maps.app.goo.gl/pTZvag2fg7ytV74eA',
+            'endereco': 'Rua Duque de Caxias, 634, Centenário, Sapiranga, RS',
             'gmaps_embed_link': 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3468.0310528904797!2d-51.01371158831997!3d-29.631841339516633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95193fbe62929801%3A0xfb885bf48d7148d6!2sAJR%20Cutelaria!5e0!3m2!1sen!2sbr!4v1703100728491!5m2!1sen!2sbr',
         }
 
@@ -147,9 +152,9 @@ class ResidencialVivaTorres(BaseLandPage):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.context = {
-            'description' : 'Landing page alugueis de temporada.',
-            'window_title': 'Residencial VivaTorres - Alugueis de Temporada',
             'static_assets_path': 'cities/torres/residencialvivatorres/assets',
+            'meta_description' : 'Landing page de alugueis de temporada em Torres Rio Grande do Sul.',
+            'window_title': 'Residencial VivaTorres - Alugueis de Temporada',
             'sobretitulo': 'Residencial Viva Torres',
             'titulo': 'Aluguéis de temporada',
             'item1_title': 'Nossa História',
@@ -159,10 +164,12 @@ class ResidencialVivaTorres(BaseLandPage):
             'item3_title': 'Nosso Diferencial',
             'item3_content': 'Oferecemos não apenas serviços e produtos de qualidade, mas também comodidade, pois disponibilizamos atendimento personalizado no seu local de preferência, seja presencial ou remoto. Renove suas ferramentas com praticidade, sem sair do seu espaço de trabalho. Agende uma visita.',
             'promocao_titulo': 'Aproveite nosso desconto exclusivo!',
-            'promocao_content': 'Informe que conheceu o nosso local através do nosso site e ganhe 10% de desconto no valor total da sua primeira compra! (Limitado à R$20)',
+            'promocao_content': 'Informe que conheceu o nosso local através do nosso site e ganhe um brinde exclusivo.',
             'horario_atendimento': 'Seg à Sex das 07-19h.',
-            'telefones': '(51) 980159178',
-            'endereco': 'Rua Duque de Caxias, 634, Centenário, Sapiranga, RS',
-            'whatslink': 'https://wa.me/+5551980159178',
-            'gmaps_embed_link': 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3468.0310528904797!2d-51.01371158831997!3d-29.631841339516633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95193fbe62929801%3A0xfb885bf48d7148d6!2sAJR%20Cutelaria!5e0!3m2!1sen!2sbr!4v1703100728491!5m2!1sen!2sbr',
+            'telefones': '(51) 982120577',
+            'email':'', 
+            'whatslink': 'https://wa.me/+5551982120577',
+            'google_share_link': 'https://maps.app.goo.gl/EsewY1fpYjL3o8dE6',
+            'endereco': 'Rua São Marcos, 335, Praia da Cal, Torres, RS',
+            'gmaps_embed_link': '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3477.7191789237595!2d-49.74209812380591!3d-29.349224675280954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9522695ce084c5a7%3A0xdcf89d2645bfa164!2sResidencial%20Viva%20Torres%3A%20Apt%20naPraia%20c%2F%20Piscina!5e0!3m2!1sen!2sbr!4v1705499583213!5m2!1sen!2sbr',
         }
