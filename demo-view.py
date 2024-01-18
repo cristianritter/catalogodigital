8e4957a8c981eab43b4f87abb80a3cffcf9d5d90
Cristian Ritter <cristianritter@gmail.com>
2024-01-18 18:28:02 -0300

    remocao de pages inacabadas

--- demo-view.py
+++ demo-view.py
@@ -1,19 +1,17 @@
 import requests
 
-#url = 'http://192.168.2.130:8000/set_demo_view/'
 url = 'https://www.mk4digital.com/set_demo_view/'
 dados_json = {
-            'static_assets_path': 'cities/florianopolis/catalogodigital/assets',
             'image0link': 'https://img.freepik.com/fotos-gratis/arranjo-lindo-de-papel-de-parede-de-flores_23-2149057015.jpg?w=740&t=st=1705514234~exp=1705514834~hmac=1713136e4e62e730ee095c34beb70f15c0bf164e0e2beaedf0dec25cbd6ec2eb',
             'image1link': 'https://img.freepik.com/fotos-gratis/flores-roxas-com-uma-bela-vista-da-ilha-da-madeira-em-portugal_181624-27202.jpg?size=626&ext=jpg&ga=GA1.1.177824122.1705512621&semt=sph',
             'image2link': 'https://img.freepik.com/fotos-gratis/flores-cor-de-rosa-em-rosa_24837-301.jpg?w=740&t=st=1705513525~exp=1705514125~hmac=a6a67ae86580b495f7b5208352f669f2c0412795cc54687c9ecb2bbe77cec712',
             'image3link': 'https://img.freepik.com/fotos-premium/as-flores-no-jardim-estao-florescendo-em-uma-bela-primavera_215913-135.jpg?w=740',            
             'meta_description' : 'Desenvolvimento e hospedagem de landing pages para negócios, marketing e divulgação organica',
             'window_title': 'Catálogo Digital - Seu negócio na Web',
-            'sobretitulo': 'Seja Bem Vindo',
-            'titulo': 'Floricultura Central',
-            'body_title': 'Seja bem vindo',
-            'body_subtitle': 'Nossa floricultura presta serviços especializados desde 2023.',
+            'sobretitulo': 'Bem Vindo ao Catálogo Digital!',
+            'titulo': 'A melhor landing page para o seu negócio',
+            'body_title': 'O que é uma landing page?',
+            'lista_servicos': ['É como a vitrine virtual do seu negócio', 'É uma página da web criada sob medida para você', 'A porta de entrada direta para o que sua empresa oferece de melhor', 'Permita que seus clientes o encontrem com os nossos serviços.'],
             'item1_title': 'Presença Online',
             'item1_content': 'Oferecemos um serviço especializado com mais de 20 anos de experiência. Serviço reconhecido por nossos amigos e clientes.',
             'item2_title': 'Credibilidade',
@@ -26,9 +24,11 @@ dados_json = {
             'telefones': '(48) 996810518',
             'whatslink': 'https://wa.me/+5551996810518',
             'email':'cristianritter@gmail.com', 
-            'google_share_link': 'https://maps.app.goo.gl/pTZvag2fg7ytV74eA',
+            'google_share_link': '',
             'endereco': '',
-            'gmaps_embed_link': ''}
+            'gmaps_embed_link': '',
+
+}
 
 
 
