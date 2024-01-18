21e543b1d0a218381efee53d38f662a70aad0edf
Cristian Ritter <cristianritter@gmail.com>
2024-01-18 12:44:51 -0300

    debug true

--- catalogodigital/settings.py
+++ catalogodigital/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-)cb+5%vn8)pa84q4#_*t(lv)5pjs)_nvo$a@iuh@p1k9$uj6t(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = ['*', '192.168.2.130']
 ALLOWED_HOSTS = ['catalogodigital.herokuapp.com']
