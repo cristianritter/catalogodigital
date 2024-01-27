6d088d663773bc83cfaca8464f5b1bf7ae5aae89
Cristian Ritter <cristianritter@gmail.com>
2024-01-27 17:27:27 -0300

    configuracao robots.txt

--- landingpages/urls.py
+++ landingpages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, re_path
 from .views import DefaultLandingPage, Homepage, set_visitas
+from django.views.generic import TemplateView
 
 app_name = 'landingpages'  
 
@@ -7,6 +8,6 @@ urlpatterns = [
 ### Urls do sistema
     path('home/', Homepage.as_view(), name='home'),
     path('set_visitas/', set_visitas, name='set_visitas'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots.txt"),
     re_path(r'^.*$', DefaultLandingPage.as_view(), name='DefaultLandingPage'),   
-  
 ]
