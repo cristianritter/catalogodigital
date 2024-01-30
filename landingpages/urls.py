22f5dd7dde0e831459b5867263ebbfc32434b508
Cristian Ritter <cristianritter@gmail.com>
2024-01-30 15:02:35 -0300

    remocao da pagina home inacabada

--- landingpages/urls.py
+++ landingpages/urls.py
@@ -9,8 +9,8 @@ custom_request = HttpRequest()
 custom_request.path = 'seja_nosso_cliente'
 
 urlpatterns = [
-    path('home/', Homepage.as_view(), name='home'),
-    path('set_visitas/', set_visitas, name='set_visitas'),
+#    path('home/', Homepage.as_view(), name='home'),
+#   path('set_visitas/', set_visitas, name='set_visitas'),
     path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots.txt"),
     re_path(r'^.*$', DefaultLandingPage.as_view(), name='DefaultLandingPage'),   
 ]
