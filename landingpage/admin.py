a76ddb64f5893fc04bcd16556c10f1c328149c75
Cristian Ritter <cristianritter@gmail.com>
2024-02-05 11:38:47 -0300

    mudancas no admin, apresentacao do ultimo acesso na guia de usuarios

--- landingpage/admin.py
+++ landingpage/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from landingpage.models import LandingPage
 from import_export.admin import ImportExportModelAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
 
 admin.site.site_title = "ConectaPages"
 admin.site.site_header = "ConectaPages"
@@ -16,3 +19,18 @@ class LandingPageAdmin(ImportExportModelAdmin):
     list_display = ['nome_empresa', 'on_air', 'url_cadastrado', 'numeros_telefone']
     search_fields = ['nome_empresa', 'url_cadastrado']
 
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'get_full_name',  'last_login', 'email' )
+    search_fields = ('username', 'get_full_name', 'email')
+    actions = None
+
+    def get_full_name(self, obj):
+        if obj.last_name and obj.first_name:
+            return f"{obj.last_name}, {obj.first_name}"
+        else:
+            return ''
+    get_full_name.short_description = 'nome_completo'
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
