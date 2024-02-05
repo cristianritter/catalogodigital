d600496f0a4378485418c1630e88bd7621e50a4b
Cristian Ritter <cristianritter@gmail.com>
2024-02-05 18:25:12 -0300

    correcao de problema com / no texto help do model de loja

--- loja/models.py
+++ loja/models.py
@@ -9,7 +9,7 @@ class Loja(PageWithBucket):
     slogam = models.CharField(max_length=100, help_text='Slogam abaixo do nome da empresa')
     titulo = models.CharField(max_length=100, help_text='Tema central na página')
     paragrafo = models.TextField(max_length=1000, help_text='Um parágrafo descrevendo a loja')
-    produtos = models.TextField(help_text='Informações do produto passadas no formato Json. Ex: {"pizza": [["Pizza de Calabresa", "Calabresa, Queijo Chedar e massa especial", 59.90, "nome_do_arquivo"]\]}')   
+    produtos = models.TextField(help_text='Informações do produto passadas no formato Json. Ex: {"pizza": ["Pizza de Calabresa", "Calabresa, Queijo Chedar e massa especial", 59.90, "nome_do_arquivo"]}')   
     def __str__(self):
         return self.url_cadastrado
     
