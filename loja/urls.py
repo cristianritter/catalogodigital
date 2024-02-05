16fbd228c3c3d4d0990ec4a1324c843c47134b18
Cristian Ritter <cristianritter@gmail.com>
2024-02-05 16:57:42 -0300

    adicao de googletag a todas as paginas do site, configuracao da url do hub de lojas

--- loja/urls.py
+++ loja/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from .views import BaseLoja, home
+from .views import HubView, LojaView, home
 from django.views.generic import TemplateView
 
 app_name = 'loja'
@@ -7,8 +7,9 @@ urlpatterns = [
   #  path('', redirect_to_landingpage, name='redirect_to_landingpage'),
   # path('cardapio_simples/', BaseLoja.as_view(), name="cardapio_simples"),
     path('', home, name='home_redirect'),
-    path('hub', TemplateView.as_view(template_name='hub.html'), name='hub_lojas'),
+ #   path('hub', TemplateView.as_view(template_name='hub.html'), name='hub_lojas'),
     path('robots.txt', TemplateView.as_view(template_name="robots-loja.txt", content_type="text/plain"), name="robots-loja.txt"),
-    re_path(r'^.*$', BaseLoja.as_view(), name='DefaultLoja'),   
+    re_path(r'^hub/(?P<pagina>.*)$', HubView.as_view(), name='Hub'),
+    re_path(r'^(?P<pagina>.*)$', LojaView.as_view(), name='Loja'),   
 
 ]
--- loja/views.py
+++ loja/views.py
@@ -1,29 +1,56 @@
-#from django.shortcuts import redirect
+from django.core.cache import cache
 from subdomains.utils import reverse
 from django.shortcuts import redirect, render
 from django.views import View
 from .models import Loja
 import json
-from django.core.cache import cache
 
 def home(request):
     return redirect(reverse('DefaultLandingPage', subdomain=None))
 
-class BaseLoja(View):
+class LojaView(View):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.context = {}
         self.template_name = 'store.html'     
-        if not cache.get('contador_lj'):
-            cache.set('contador_lj', 0, timeout=None)
          
-    def get(self, request, *args, **kwargs):
-        url_recebida = request.path.replace('/','')
-        loja__data = Loja.objects.filter(url_cadastrado=url_recebida).first()
+    def get(self, request, pagina, *args, **kwargs):
+        loja__data = Loja.objects.filter(url_cadastrado=pagina[:-1:]).first()
         if loja__data and loja__data.on_air:
             self.context = {
                 'meta_description': loja__data.meta_description,
-                'endereco_bucket': loja__data.endereco_bucket+url_recebida+'/store/',
+                'endereco_bucket': loja__data.endereco_bucket+pagina+'store/',
+                'nome_empresa': loja__data.nome_empresa,
+                'link_whats': loja__data.link_whats,
+                'link_facebook': loja__data.link_facebook,
+                'link_instagram': loja__data.link_instagram,
+                'slogam': loja__data.slogam,
+                'titulo': loja__data.titulo,
+                'paragrafo': loja__data.paragrafo,
+                'produtos': json.loads(loja__data.produtos),
+            } 
+        else:
+            return render(request, '404-wall-e.html')  
+        visitas = 1 + cache.get('visitas_lojas')
+        cache.set('visitas_lojas', visitas, timeout=None)
+        self.context['contador_visitas'] = visitas
+        return render(request, self.template_name, self.context)
+
+class HubView(View):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.context = {}
+        self.template_name = 'hub.html'     
+        if not cache.get('contador_lj'):
+            cache.set('contador_lj', 0, timeout=None)
+         
+    def get(self, request, pagina, *args, **kwargs):
+        print('hub', pagina)
+        loja__data = Loja.objects.filter(url_cadastrado=pagina).first()
+        if loja__data and loja__data.on_air:
+            self.context = {
+                'meta_description': loja__data.meta_description,
+                'endereco_bucket': loja__data.endereco_bucket+pagina+'/store/',
                 'nome_empresa': loja__data.nome_empresa,
                 'link_whats': loja__data.link_whats,
                 'link_facebook': loja__data.link_facebook,
