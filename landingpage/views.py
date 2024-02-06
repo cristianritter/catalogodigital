5e77f755565a76d76d1da95a23780f8211e7f1eb
Cristian Ritter <cristianritter@gmail.com>
2024-02-06 08:37:49 -0300

    testes com nova landingpage, remocao da consulta ao banco de dados

--- landingpage/views.py
+++ landingpage/views.py
@@ -90,39 +90,39 @@ class NewLandingPage(View):
         super().__init__(*args, **kwargs)
         self.context = {}
         self.template_name = 'new_landing_page.html'
+        self.landing_page_data = LandingPage.objects.filter(url_cadastrado='ajr_cutelaria').first()
       
     def get(self, request, *args, **kwargs):
-        url_recebida = 'ajr_cutelaria'
-        landing_page_data = LandingPage.objects.filter(url_cadastrado=url_recebida).first()
-        if landing_page_data and landing_page_data.on_air:
+        url_recebida='ajr_cutelaria'
+        if self.landing_page_data and self.landing_page_data.on_air:
             try:
-                lista_items = json.loads(landing_page_data.lista_items)
+                lista_items = json.loads(self.landing_page_data.lista_items)
             except:
                 return HttpResponse("AVISO: Revise a construção da seção 'Lista items' na página de administração.")
             try:
-                dados_dict = json.loads(landing_page_data.colunas_items)
+                dados_dict = json.loads(self.landing_page_data.colunas_items)
             except:
                 return HttpResponse("AVISO: Revise a construção da seção 'Coluna items' na página de administração.")
            
             self.context = {
-                'endereco_bucket': landing_page_data.endereco_bucket+url_recebida+'/',
-                'nomes_arquivos_imagens': landing_page_data.nomes_arquivos_imagens.split(','),
-                'nome_empresa': landing_page_data.nome_empresa,
-                'descricao_curta': landing_page_data.descricao_curta,
-                'meta_description': landing_page_data.meta_description,
-                'lista_titulo': landing_page_data.lista_titulo,
+                'endereco_bucket': self.landing_page_data.endereco_bucket+url_recebida+'/',
+                'nomes_arquivos_imagens': self.landing_page_data.nomes_arquivos_imagens.split(','),
+                'nome_empresa': self.landing_page_data.nome_empresa,
+                'descricao_curta': self.landing_page_data.descricao_curta,
+                'meta_description': self.landing_page_data.meta_description,
+                'lista_titulo': self.landing_page_data.lista_titulo,
                 'lista_items': lista_items,
                 'dados_dict': dados_dict,
-                'numeros_telefone': landing_page_data.numeros_telefone,
-                'email_contato': landing_page_data.email_contato,
-                'endereco': landing_page_data.endereco,
-                'horario_atendimento': landing_page_data.horario_atendimento,
-                'link_whats': landing_page_data.link_whats,
-                'link_instagram': landing_page_data.link_instagram,
-                'link_facebook': landing_page_data.link_facebook,
-                'reviews_link': landing_page_data.reviews_link,
-                'gmaps_link': landing_page_data.gmaps_link,
-                'link_loja': landing_page_data.link_loja,
+                'numeros_telefone': self.landing_page_data.numeros_telefone,
+                'email_contato': self.landing_page_data.email_contato,
+                'endereco': self.landing_page_data.endereco,
+                'horario_atendimento': self.landing_page_data.horario_atendimento,
+                'link_whats': self.landing_page_data.link_whats,
+                'link_instagram': self.landing_page_data.link_instagram,
+                'link_facebook': self.landing_page_data.link_facebook,
+                'reviews_link': self.landing_page_data.reviews_link,
+                'gmaps_link': self.landing_page_data.gmaps_link,
+                'link_loja': self.landing_page_data.link_loja,
             } 
         else:
             return render(request, '404-wall-e.html')  
