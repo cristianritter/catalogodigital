c343722b17442575cc8922688548ded9b60c7544
Cristian Ritter <cristianritter@gmail.com>
2024-02-15 16:32:59 -0300

    adicionada validacao de dados ao criar model

--- landingpage/models.py
+++ landingpage/models.py
@@ -1,33 +1,67 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+import json
 
 class Page(models.Model):
     class Meta:
         abstract = True  # Define essa classe como abstrata para que não seja criada como tabela no banco de dados
-    on_air = models.BooleanField(help_text='Indica se a página está no ar.')
+    on_air = models.BooleanField(default=False, help_text='Indica se a página está no ar.')
     url = models.CharField(max_length=50, help_text='A parte personalizada do endereço no final do link da página')
     meta_description = models.CharField(blank=True, max_length=160, help_text='Descrição do site que vai aparecer para o usuário durante a busca.')
-    link_whats = models.URLField(blank=True, help_text='https://api.whatsapp.com/send?phone=5199876543')
+    link_whats = models.URLField(blank=True, help_text='https://api.whatsapp.com/send?phone=')
     link_facebook = models.URLField(blank=True, help_text='Link para a página do facebook')
     link_instagram = models.URLField(blank=True, help_text='Link para a página do instagram')
     nome_empresa = models.CharField(max_length=50, help_text='Nome da empresa, da loja ou do concentrador')
+
+    def clean(self):
+        if ' ' in self.url:
+            raise ValidationError(f'O conteúdo de "Url: {self.url}" não pode conter espaços em branco.')
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(Page, self).save(*args, **kwargs)
 
 class LandingPage(Page):
     class Meta:
         verbose_name = 'Registro de Landing Page'
         verbose_name_plural = 'Registros de Landing Pages'
     carousel_size = models.IntegerField(help_text='Quantidade de imagens no carossel da página.')
+
+    def clean(self):
+        try:
+            if self.lista_items:
+                json.loads(self.lista_items)
+        except:
+            raise ValidationError(f'O conteúdo de "Lista items" está incorreto.')
+        try:
+            if self.colunas_items:
+                json.loads(self.colunas_items)
+        except:
+            raise ValidationError(f'O conteúdo de "Colunas items" está incorreto.')
+        try:
+            if self.link_loja:
+                json.loads(self.link_loja)
+        except:
+            raise ValidationError(f'O conteúdo de "Link loja" está incorreto.')
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(Page, self).save(*args, **kwargs)
+
     #Dados Gerais da empresa
     descricao_curta = models.CharField(max_length=100, help_text='Texto acima do nome da empresa(SubHeader). Síntese da atividade principal. (Obrigatório conter as PALAVRAS CHAVES)')
+    trend_words = models.CharField(max_length=300, help_text='Palavras chaves consultadas no Google Trend')
     resumo_chave = models.TextField(max_length=300, help_text='Este resumo deve ser uma descrição bem clara e objetiva (Obrigatório conter variações das PALAVRAS CHAVES e o NOME DA CIDADE).')
-    lista_items = models.TextField(blank=True, max_length=600,  help_text='Seção de lista da página. Dict no formato {"Título da lista": ["item1","item2",...]')
-    colunas_items = models.TextField(blank=True, help_text='SEeão de colunas da página. Dict no formato {"Título1":"Conteúdo1","Título2":"Conteúdo2"},...')
+    lista_items = models.TextField(default='{}', max_length=600,  help_text='Seção de lista da página. Dict no formato {"Título da lista": ["item1","item2",...]')
+    colunas_items = models.TextField(default='{}', help_text='SEeão de colunas da página. Dict no formato {"Título1":"Conteúdo1","Título2":"Conteúdo2"},...')
     numeros_telefone = models.CharField(blank=True, max_length=50, help_text='Ex: (12) 98765 4321 (São permitidos múltiplos números)')
     email_contato = models.EmailField(blank=True, help_text='Ex: nome@empresa.com.br')
     endereco = models.CharField(blank=True, max_length=100, help_text="Ex: Rua Duque de Caxias, 237")
     cidade = models.CharField(max_length=50)
     horario_atendimento = models.CharField(blank=True, max_length=100, help_text='Ex: Seg à Sex das 10h as 17h.')
+    
     # Links (usando URLField)
-    link_loja = models.TextField(max_length=150, blank=True, help_text='Nome do botão e link para para site externo. Dict no formato {"Conheça nossa Loja Virtual":"https://minhaloja.com.br" }')
+    link_loja = models.TextField(default='{}', max_length=150, blank=True, help_text='Nome do botão e link para para site externo. Dict no formato {"Conheça nossa Loja Virtual":"https://minhaloja.com.br" }')
     reviews_link = models.URLField(blank=True, help_text="Link obtido abrindo o google empresas e clicando em Share > Send a link. Ex: https://maps.app.goo.gl/pTZvag2fg7ytV74eA")
     gmaps_link = models.CharField(blank=True, max_length=500, help_text="Link obtido abrindo o google empresas e clicando em Share > Embed a map > Small.")
     def __str__(self):
