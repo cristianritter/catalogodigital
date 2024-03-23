e882e5f48c9d539a4bb23a51565d5768dc1aca87
Cristian Ritter <cristianritter@gmail.com>
2024-03-23 12:58:03 -0300

    fixed uploading image without selec type of image

--- landingpage/forms.py
+++ landingpage/forms.py
@@ -15,18 +15,23 @@ class LandingPageForm(forms.ModelForm):
         model = LandingPage
         fields = '__all__'
 
-    uploadType = forms.ChoiceField(choices=[('cover', 'Imagem de Capa'), ('carousel', 'Imagem de Carrosel')], label='O que você está enviando?')
+    uploadType = forms.ChoiceField(choices=[('nothing', 'Nenhum arquivo'), ('cover', 'Imagem de Capa'), ('carousel', 'Imagem de Carrosel')], label='O que você está enviando?', 
+                                   help_text="Imagens de carrousel devem ser enviadas em sequencia, numeradas no nome do arquivo a partir de 2")
     fileUploadField = forms.ImageField(required=False, label='Upload de arquivo')
 
-    def clean_fileUpload(self):    
-        numberMaxOfFilesOnDestination = 6
+    def clean(self):
+        cleaned_data = super().clean()
+        upload_type = cleaned_data.get('uploadType')
+        file_upload = cleaned_data.get('fileUploadField')
+        number_max_of_files = 6
         
-        filesAlreadyOnDestCounter = len(Storage.get_bucket_file_list(self.destFolder))
-        print(filesAlreadyOnDestCounter)
-        if ( filesAlreadyOnDestCounter >= numberMaxOfFilesOnDestination and self.cleaned_data[self.fileUploadField]): 
-            raise forms.ValidationError(f'O número máximo de arquivos permitidos neste diretório é {numberMaxOfFilesOnDestination}')
-        self.save()
-            
+        if upload_type == 'nothing' and file_upload:
+            raise forms.ValidationError('Selecione o tipo de imagem que está enviando.')
+
+        files_already_on_dest_counter = len(Storage.get_bucket_file_list(self.destFolder))
+        if files_already_on_dest_counter >= number_max_of_files and file_upload:
+            raise forms.ValidationError(f'O número máximo de arquivos permitidos neste diretório é {number_max_of_files}')
+
     def save(self, commit=True):
         instance = super().save(commit=False)
         if not self.cleaned_data[self.fileUploadField]:
