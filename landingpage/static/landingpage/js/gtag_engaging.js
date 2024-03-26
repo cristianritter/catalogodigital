d003b0157713558c8d27449dd48fa51f3651482b
Cristian Ritter <cristianritter@gmail.com>
2024-03-25 21:05:53 -0300

    changes at gtag

--- landingpage/static/landingpage/js/gtag_engaging.js
+++
@@ -1,8 +0,0 @@
-    document.getElementById('chamar-no-whatsapp-button').addEventListener('click', function() 
-    {
-        // Envia um evento personalizado para o Google Analytics quando o botão é clicado
-        gtag('event', 'clique_em_chamar_no_whats', {
-            'event_category': 'clique_em_chamar_no_whats',
-            'event_label': 'clique_em_chamar_no_whats Clicado'
-        });
-    });
